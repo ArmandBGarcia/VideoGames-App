9139fbe2dea5db7e6044fedc96ddd0b718fbbdbf
ArmandoBGarcia <xdarcx@hotmail.es>
2022-09-21 18:28:44 -0500

    contact form terminado

--- client/src/pages/ContactForm.jsx
+++ client/src/pages/ContactForm.jsx
@@ -1,24 +1,73 @@
 import React from "react";
+import emailjs from "emailjs-com";
 import s from "./styles/ContactForm.module.css";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_xn7cyqm",
+        "template_fduzfm9",
+        e.target,
+        "7wkI-pqxkp4uugnlP"
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          alert("Message sent successfully");
+        } else alert("Message not sent, something went wrong");
+      });
+    setForm({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
   return (
     <div className={s.container}>
       <div className={s.containerForm}>
         <h3 className={s.title}>Contact Form</h3>
         <hr />
-        <form action="">
+        <form onSubmit={sendEmail}>
           <div>
             <label>
               <p>Nombre</p>
             </label>
-            <input type="text" id="name" name="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={handleChange}
+              value={form.name}
+              required
+            />
           </div>
           <div>
             <label>
               <p>Email</p>
             </label>
-            <input type="text" id="email" name="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              value={form.email}
+              required
+            />
           </div>
           <div>
             <label>
@@ -26,8 +75,11 @@ const ContactForm = () => {
             </label>
             <textarea
               className={s.textarea}
-              name="mesagge"
+              name="message"
               id="message"
+              onChange={handleChange}
+              value={form.message}
+              required
             ></textarea>
           </div>
           <button className={s.btn} type="submit">
